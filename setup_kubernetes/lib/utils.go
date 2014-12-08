5953efc949c84f52a98d91a5c981f960d8b8c888
Mike Metral <metral@gmail.com>
2014-12-08 15:50:01 -0800

    make valid var type

--- setup_kubernetes/lib/utils.go
+++ setup_kubernetes/lib/utils.go
@@ -55,7 +55,7 @@ func httpGetRequest(url string) []byte {
 }
 
 func httpPutRequest(urlStr string, data interface{}, dataIsJSON bool) *http.Response {
-	var req = nil
+	var req http.NewRequest
 
 	switch dataIsJSON {
 	case true:
